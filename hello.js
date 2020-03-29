1648731b62ffa8aeab0634213f477750247b5b0d
Kinnary raichura <kraichura@LT-C02S68FLG8WP.local>
2020-03-29 03:56:12 -0400

    Initial commit

---
+++ hello.js
@@ -0,0 +1,10 @@
+exports.lambdaHandler = async (event, context) => {
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: 'hello world',
+        })
+    };
+
+    return response;
+};
